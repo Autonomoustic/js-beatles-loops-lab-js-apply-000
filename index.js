f22c2b10b6a76fd9ff314a1cfabd359dcb832195
ali aziz <aa_connect@outlook.com>
2018-07-09 10:33:54 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -1,1 +1,6 @@
 // add solution here
+function theBeatlesPlay(musician, instruments) {
+  for (var i = 0, l = theBeatlesPlay.length; i < l; i++) {
+    console.log (`$[musicians] plays $[instruments]`)
+  }
+}
